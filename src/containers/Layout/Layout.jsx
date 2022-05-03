06790a2b0a7fff3f3256ca83a8f5468c2662340b
Tohidul Islam <mdtohid.sust@gmail.com>
2022-05-04 00:13:26 +0600

    trial

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -6,12 +6,121 @@ function Layout() {
   return (
     <div id="container" className="flex flex-col">
       <Header />
-      <div className="flex flex-1 mobileRange:flex-col">
+      <div className="flex flex-1 mobileRange:flex-col ">
         <div className="mobileRange:order-2 bg-cyan-300 ">
           <Navbar />
         </div>
-        <div className="flex-1 bg-green-400 overflow-auto">
-          <Outlet />
+        <div className="flex-1 bg-green-400 ">
+          {/* <Outlet /> */}
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
+          ratione debitis natus, totam repellat tenetur illum eveniet asperiores
+          in repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus? Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Laboriosam ratione debitis natus, totam repellat
+          tenetur illum eveniet asperiores in repudiandae distinctio similique
+          laborum dicta provident. Eum earum iste magnam doloribus?Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. Laboriosam ratione
+          debitis natus, totam repellat tenetur illum eveniet asperiores in
+          repudiandae distinctio similique laborum dicta provident. Eum earum
+          iste magnam doloribus?
         </div>
       </div>
     </div>
